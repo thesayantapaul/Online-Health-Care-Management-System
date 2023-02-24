1c9d716bbc46cbc347c98a4d54aa9b11c55dd5cd
Sayanta Paul <71244312+thesayantapaul@users.noreply.github.com>
2023-02-24 13:02:40 +0530

    patient model added

---
+++ src/java/com/exavalu/models/Patients.java
@@ -0,0 +1,128 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.exavalu.models;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import java.io.Serializable;
+import java.util.Map;
+import org.apache.struts2.dispatcher.ApplicationMap;
+import org.apache.struts2.dispatcher.SessionMap;
+import org.apache.struts2.interceptor.ApplicationAware;
+import org.apache.struts2.interceptor.SessionAware;
+
+/**
+ *
+ * @author SAYANTA PAUL
+ */
+public class Patients extends ActionSupport implements ApplicationAware, SessionAware, Serializable{
+    
+    private String patientId,patientFirstName,patientLastName,age,gender,appointmentId;
+
+    private SessionMap<String, Object> sessionmap = (SessionMap<String, Object>) ActionContext.getContext().getSession();
+
+    private ApplicationMap map = (ApplicationMap) ActionContext.getContext().getApplication();
+
+    @Override
+    public void setApplication(Map<String, Object> application) {
+        setMap((ApplicationMap) application);
+    }
+
+    @Override
+    public void setSession(Map<String, Object> session) {
+        setSessionmap((SessionMap<String, Object>) (SessionMap) session);
+    }
+    /**
+     * @return the patientId
+     */
+    public String getPatientId() {
+        return patientId;
+    }
+
+    /**
+     * @param patientId the patientId to set
+     */
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    /**
+     * @return the patientFirstName
+     */
+    public String getPatientFirstName() {
+        return patientFirstName;
+    }
+
+    /**
+     * @param patientFirstName the patientFirstName to set
+     */
+    public void setPatientFirstName(String patientFirstName) {
+        this.patientFirstName = patientFirstName;
+    }
+
+    /**
+     * @return the patientLastName
+     */
+    public String getPatientLastName() {
+        return patientLastName;
+    }
+
+    /**
+     * @param patientLastName the patientLastName to set
+     */
+    public void setPatientLastName(String patientLastName) {
+        this.patientLastName = patientLastName;
+    }
+
+    /**
+     * @return the age
+     */
+    public String getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the appointmentId
+     */
+    public String getAppointmentId() {
+        return appointmentId;
+    }
+
+    /**
+     * @param appointmentId the appointmentId to set
+     */
+    public void setAppointmentId(String appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+      private void setMap(ApplicationMap applicationMap) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void setSessionmap(SessionMap<String, Object> sessionMap) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
